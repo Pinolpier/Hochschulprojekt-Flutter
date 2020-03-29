4f7bd6c978870232b40219d823c6dbed3e2e9ceb
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2020-03-29 20:51:10 +0200

    adding design for test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:aiblabswp2020ssunivents/start.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:aiblabswp2020ssunivents/main.dart';
