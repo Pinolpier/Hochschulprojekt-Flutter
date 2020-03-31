99e317524c8914ffe4c96f7b5784f7e7fbc438bc
Mathias Darscht <mdarscht@stud.hs-heilbronn.de>
2020-03-31 09:08:57 +0200

    configuring the navigation bar

--- lib/homeFeedScreenUI/navigationBarUI.dart
+++ lib/homeFeedScreenUI/navigationBarUI.dart
@@ -1,12 +1,20 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:aiblabswp2020ssunivents/homeFeedScreenUI/feed.dart';
 
 //@author mdarscht
-class NavigationBarUI extends StatelessWidget {
+class NavigationBarUI extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => NavigationBarUIControl();
+}
+
+class NavigationBarUIControl extends State<NavigationBarUI> {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return DefaultTabController(
       length: 5,
       child: Scaffold(
@@ -14,13 +22,24 @@ class NavigationBarUI extends StatelessWidget {
           backgroundColor: Color(0xff), //TODO: color definition
           title: Center(child: Text('Univents', ),),
           bottom: TabBar(
-              tabs: <Widget>[
-                Tab(child: Icon(Icons.event_note)),
-                Tab(child: Icon(Icons.map)),
-                Tab(child: Icon(Icons.people)),
-                Tab(child: Icon(Icons.account_circle)),
-                Tab(child: Icon(Icons.settings)),
-              ]
+            onTap: _something,
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.map),
+              ),
+              Tab(
+                icon: Icon(Icons.group),
+              ),
+              Tab(
+                icon: Icon(Icons.account_circle),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+            ],
           ),
         ),
         body: ListView(
@@ -28,5 +47,50 @@ class NavigationBarUI extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _something(int index) {
+    switch (index) {
+      case 0: {
+        _toHome();
+      }
+      break;
+      case 1: {
+        _toMap();
+      }
+      break;
+      case 2: {
+        _toFiends();
+      }
+      break;
+      case 3: {
+        _toProfile();
+      }
+      break;
+      case 4: {
+        _toSettings();
+      }
+      break;
+    }
+  }
+
+  Route _toHome() {
+    print('navigate to home');
+  }
+
+  Route _toMap() {
+    print('navigate to map');
+  }
+
+  Route _toFiends() {
+    print('navigate to friends');
+  }
+
+  Route _toProfile() {
+    print('navigate to profile');
+  }
+
+  Route _toSettings() {
+    print('navigate to settings');
   }
 }
