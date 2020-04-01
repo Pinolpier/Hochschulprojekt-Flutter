da08b5fb347fcc0bf6f345e591bf56853c3d3f60
chhenri <chhenri@stud.hs-heilbronn.de>
2020-04-01 15:19:46 +0200

    initial commit for settingsscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:aiblabswp2020ssunivents/settings_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:aiblabswp2020ssunivents/start.dart';
+import 'package:aiblabswp2020ssunivents/option_model.dart';
 
 void main() => runApp(MyApp());
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Select Option UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.black),
+      home: SettingsScreen(),
+    );
+  }
+}
