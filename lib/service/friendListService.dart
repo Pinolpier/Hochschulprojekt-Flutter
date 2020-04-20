9a3e4aa3b23757ee8830bd2136d962440bdbf0ef
markus <mhaering@stud.hs-heilbronn.de>
2020-04-21 00:23:09 +0200

    first Try of FriendStructure in database

--- lib/service/friendListService.dart
+++ lib/service/friendListService.dart
@@ -1,15 +1,45 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:univents/Controller/backendAPI.dart';
+import 'package:univents/Controller/authService.dart';
+import 'package:univents/Controller/userProfileService.dart';
+import 'package:univents/model/friendGroup.dart';
 
-final firebaseInstance = Firestore.instance
-    .collection(COLLECTION)
-    .document(getUidOfCurrentlySignedInUser());
 final String COLLECTION = 'friends';
+final firebaseInstance = Firestore.instance;
 
-void addFriend(String nutzerID) async {}
+void addFriendbyEmail(String email) async {
+  String uid = await getUidOfCurrentlySignedInUser();
+  String friendID = await getUidFromEmail(email);
+  Map<String, String> friendMap;
+  firebaseInstance.collection(COLLECTION).document(uid).updateData(friendMap);
+}
 
-List<String> getFriends() {}
+void addFriendbyUsername(String username) async {
+  String uid = await getUidOfCurrentlySignedInUser();
+  String friendid = await getUidFromUserName(username);
+  Map<String, List<String>> dataMap;
+  dataMap['friends'] = [friendid];
+  firebaseInstance.collection(COLLECTION).document(uid).updateData(dataMap);
+}
 
-void getFriendsbyGroups() {}
+void removeFriend(String friendid) async {
+  firebaseInstance.collection(COLLECTION).document(
+      getUidOfCurrentlySignedInUser());
+}
 
-void groupFriend(String userID) {}
+Future<List<String>> getFriends() async {
+  String uid = await getUidOfCurrentlySignedInUser();
+  DocumentSnapshot documentSnapshot = await firebaseInstance.collection(
+      COLLECTION).document(uid).get();
+}
+
+void getFriendsbyGroups() async {
+
+}
+
+void groupFriend(String userID, String groupName,
+    FriendGroup friendGroup) async {
+  String uid = await getUidOfCurrentlySignedInUser();
+  Map<String, List<String>> groupMap;
+  groupMap[groupName] = [userID];
+  firebaseInstance.collection(COLLECTION).document(uid).updateData(groupMap);
+}
