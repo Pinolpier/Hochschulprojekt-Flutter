f672328bbe7bd7427e305e474c9af14ff9857ea6
chhenri <chhenri@stud.hs-heilbronn.de>
2020-04-21 13:28:42 +0200

    hotfix for profilescreen, deleted ForeignProfileScreen class and adapted profile_screen to remove code duplicates

--- lib/Controller/main.dart
+++ lib/Controller/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:univents/View/profile_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter create event UI',
+      home: ProfileScreen(),
     );
   }
 }
