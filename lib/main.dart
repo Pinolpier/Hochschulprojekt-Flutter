7c07de1a8845e762cf852a313589606f5b3d666d
mhaering <mhaering@stud.hs-heilbronn.de>
2020-05-19 10:46:38 +0200

    added dart doc

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:univents/controller/authService.dart';
 import 'package:univents/service/app_localizations.dart';
-import 'package:univents/view/createEvent_screen.dart';
+import 'package:univents/view/login_screen.dart';
 
 void main() {
   runApp(new MaterialApp(home: UniventsApp()));
@@ -41,7 +41,7 @@ class UniventsApp extends StatelessWidget {
             }
             return supportedLocales.first;
           },
-          home: CreateEventScreen(),
+          home: LoginScreen(),
         ));
   }
 }
