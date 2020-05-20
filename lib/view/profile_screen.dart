27ad327970ee4675fcf0de78a10f9fb00487bd0a
mhaering <mhaering@stud.hs-heilbronn.de>
2020-05-20 16:21:42 +0200

    changed Toast in Profile-Screen into interface method

--- lib/view/profile_screen.dart
+++ lib/view/profile_screen.dart
@@ -2,12 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:univents/controller/authService.dart';
 import 'package:univents/controller/userProfileService.dart';
 import 'package:univents/model/userProfile.dart';
 import 'package:univents/service/app_localizations.dart';
 import 'package:univents/service/utils/imagePickerUnivents.dart';
+import 'package:univents/service/utils/toast.dart';
 
 import 'dialogs/DialogHelper.dart';
 
@@ -198,15 +198,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                               await updateImage(profilepic, userProfile);
                             }
                             else {
-                              Fluttertoast.showToast(
-                                  msg: AppLocalizations.of(context).translate('profile_screen_toast'),
-                                  toastLength: Toast.LENGTH_SHORT,
-                                  gravity: ToastGravity.CENTER,
-                                  timeInSecForIosWeb: 1,
-                                  backgroundColor: Colors.red,
-                                  textColor: Colors.white,
-                                  fontSize: 16.0
-                              );
+                              show_short_toast(AppLocalizations.of(context).translate('profile_screen_toast'));
                             }
                           },
                           child: Center(
