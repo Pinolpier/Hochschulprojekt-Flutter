3dcbc1dc56329fccc7dbc7fac9da84692e313f4f
chhenri <chhenri@stud.hs-heilbronn.de>
2020-05-21 11:21:46 +0200

    added univents logo for loginscreen and removed TODO

--- lib/service/utils/errorDialogCreator.dart
+++ lib/service/utils/errorDialogCreator.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:univents/service/app_localizations.dart';
 
 class ErrorDialogCreator extends StatelessWidget {
   ErrorDialogCreator(String title, String body, bool prio) {
