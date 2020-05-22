920f3c327447fa8e9533ece8e5f440b639be8c69
eoao <mdarscht@stud.hs-heilbronn.de>
2020-05-22 11:10:50 +0200

    resolved color problem and bracket problem

--- lib/view/friendList_screen.dart
+++ lib/view/friendList_screen.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:univents/model/FriendslistDummies.dart';
 import 'package:univents/model/GroupDummies.dart';
+import 'package:univents/model/colors.dart';
 import 'package:univents/service/app_localizations.dart';
-import 'package:univents/service/utils/errorDialogCreator.dart';
 import 'package:univents/view/dialogs/Debouncer.dart';
 import 'package:univents/view/dialogs/DialogHelper.dart';
 
-class FriendlistScreen extends StatefulWidget{
+class FriendlistScreen extends StatefulWidget {
   @override
   _FriendlistScreenState createState() => _FriendlistScreenState();
 }
@@ -16,8 +16,7 @@ class FriendlistScreen extends StatefulWidget{
  * this class creates a friendslist with a searchbar at the top to filter through the friends (not implemented yet) and a
  * button at the bottom to add new friends, also used to display groups depending on the bool [isFriendsScreen] to avoid code duplication!
  */
-class _FriendlistScreenState extends State<FriendlistScreen>{
-
+class _FriendlistScreenState extends State<FriendlistScreen> {
   final _debouncer = new Debouncer(500);
   bool isFriendsScreen = true;
 
@@ -32,95 +31,121 @@ class _FriendlistScreenState extends State<FriendlistScreen>{
 
   List<GroupDummies> groups = [
     GroupDummies(name: "GROUP1", profilepic: "mango.png")
-    ];
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Scaffold(
-        backgroundColor: Colors.grey[200],
-        body: Column(
-          children: <Widget>[
-            TextField(
+    return Scaffold(
+      backgroundColor: univentsLightGreyBackground,
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: isFriendsScreen == true
+            ? Text(AppLocalizations.of(context).translate('your_friendsList'))
+            : Text(AppLocalizations.of(context).translate('your_groups')),
+        leading: isFriendsScreen == true
+            ? null
+            : new IconButton(
+                icon: new Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    isFriendsScreen = true;
+                  });
+                },
+              ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
               decoration: InputDecoration(
                 contentPadding: EdgeInsets.all(10.0),
-                hintText: isFriendsScreen == true ? AppLocalizations.of(context)
-                    .translate('search_for_friend') : AppLocalizations.of(context)
-                    .translate('search_for_group'),
+                hintText: isFriendsScreen == true
+                    ? AppLocalizations.of(context)
+                        .translate('search_for_friend')
+                    : AppLocalizations.of(context)
+                        .translate('search_for_group'),
               ),
               onChanged: (string) {
                 //debouncer makes sure the user input only gets registered after 500ms to give the user time to input the full search query
                 _debouncer.run(() {
                   print(string);
                 });
-              }
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: isFriendsScreen == true ? friends.length : groups.length,
-                itemBuilder: (context, index){
+              }),
+          Expanded(
+            child: ListView.builder(
+                itemCount:
+                    isFriendsScreen == true ? friends.length : groups.length,
+                itemBuilder: (context, index) {
                   return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 6.0),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 1.0, horizontal: 6.0),
                     child: Card(
                       child: ListTile(
                         onTap: () {
                           setState(() {
-                            isFriendsScreen == true ? print(friends[index].name + " was pressed"): isFriendsScreen = true; //Beim Auswählen einer Gruppe öffnet sich der eigene FriendsList_Screen (wir noch geändert sobald Backend steht)
+                            isFriendsScreen == true
+                                ? print(friends[index].name + " was pressed")
+                                : isFriendsScreen =
+                                    true; //Beim Auswählen einer Gruppe öffnet sich der eigene FriendsList_Screen (wir noch geändert sobald Backend steht)
                           });
                         },
-                        title: isFriendsScreen == true ? Text(friends[index].name) : Text(groups[index].name),
+                        title: isFriendsScreen == true
+                            ? Text(friends[index].name)
+                            : Text(groups[index].name),
                         leading: GestureDetector(
                           behavior: HitTestBehavior.opaque,
-                          onTap: () {
-                          },
+                          onTap: () {},
                           child: CircleAvatar(
-                            backgroundImage: isFriendsScreen == true ? AssetImage('assets/${friends[index].profilepic}') : AssetImage('assets/${groups[index].profilepic}'),  //TODO Gruppenvorschaubild ändern können ? Rücksprache mit PO Markus Link
+                            backgroundImage: isFriendsScreen == true
+                                ? AssetImage(
+                                    'assets/${friends[index].profilepic}')
+                                : AssetImage(
+                                    'assets/${groups[index].profilepic}'), //TODO Gruppenvorschaubild ändern können ? Rücksprache mit PO Markus Link
                           ),
                         ),
                       ),
                     ),
                   );
-                }
-              ),
-            ),
-            isFriendsScreen == true ? Column(
-              children: <Widget>[
-                Padding(
+                }),
+          ),
+          isFriendsScreen == true
+              ? Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 340.0, bottom: 5.0),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            isFriendsScreen = false;
+                          });
+                        },
+                        child: Icon(Icons.group),
+                        backgroundColor: primaryColor,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 340.0, bottom: 5.0),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          showAddFriendsDialog(context);
+                        },
+                        child: Icon(Icons.group_add),
+                        backgroundColor: primaryColor,
+                      ),
+                    ),
+                  ],
+                )
+              : Padding(
                   padding: const EdgeInsets.only(left: 340.0, bottom: 5.0),
                   child: FloatingActionButton(
                     onPressed: () {
-                      setState(() {
-                        isFriendsScreen = false;
-                      });
+                      showFriendsDialog(context);
                     },
-                    child: Icon(Icons.group),
-                    backgroundColor: Colors.blueAccent,
+                    child: Icon(Icons.add),
+                    backgroundColor: primaryColor,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 340.0, bottom: 5.0),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      showAddFriendsDialog(context);
-                    //showErrorDialog(context, "Test1", "test2", true);},
-                    child: Icon(Icons.group_add),
-                    backgroundColor: Colors.blueAccent,
-                  ),
-                ),
-              ],
-            )
-            : Padding(
-              padding: const EdgeInsets.only(left: 340.0, bottom: 5.0),
-              child: FloatingActionButton(
-                onPressed: () {
-                  showFriendsDialog(context);
-                },
-                child: Icon(Icons.add),
-                backgroundColor: Colors.blueAccent,
-              ),
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
