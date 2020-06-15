38620f0d829232db02890efcc3269aafe6b76ff4
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-15 16:07:20 +0200

    corrected feed_filter_values.dart documentation

--- lib/view/homeFeed_screen/feed_filter_values.dart
+++ lib/view/homeFeed_screen/feed_filter_values.dart
@@ -3,14 +3,20 @@ import 'package:univents/service/app_localizations.dart';
 
 import 'feed_filter.dart';
 
+/// todo: add author
+/// todo: CONSIDER writing a library-level doc comment
+
 class FeedFilterValues {
   /// value to translate
   final FeedFilter _filterValue;
 
+  /// todo: missing documenation of constructor
   const FeedFilterValues(this._filterValue);
 
+  /// todo: missing documentation
   FeedFilter get filterValue => _filterValue;
 
+  /// todo: DO separate the first sentence of a doc comment into its own paragraph.
   /// translates the value into the right language
   /// (BuildContext)[context] for setting the translation
   String convertToString(BuildContext context) {
