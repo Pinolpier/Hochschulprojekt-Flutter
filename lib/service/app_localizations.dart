4a2e76b00e890585943758c098db2e37ccb4d4ce
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-15 14:57:46 +0200

    corrected app_localizations.dart documentation

--- lib/service/app_localizations.dart
+++ lib/service/app_localizations.dart
@@ -4,9 +4,15 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+/// todo: add author
+/// todo: CONSIDER writing a library-level doc comment
+/// todo: reorganize: class -> attributes -> contructor -> methods
+
 class AppLocalizations {
+  /// todo: add documentation to variables
   final Locale locale;
 
+  /// todo: missing documentation of constructor
   AppLocalizations(this.locale);
 
   static const LocalizationsDelegate<AppLocalizations> delegate =
@@ -18,6 +24,7 @@ class AppLocalizations {
 
   Map<String, String> _localizedStrings;
 
+  /// todo: missing documentation
   Future<bool> load() async {
     String jsonString =
         await rootBundle.loadString('language/${locale.languageCode}.json');
@@ -28,21 +35,25 @@ class AppLocalizations {
     return true;
   }
 
+  /// todo: missing documentation
   String translate(String key) {
     return _localizedStrings[key];
   }
 }
 
+/// todo: missing documentation
 // LocalizationsDelegate is a factory for a set of localized resources
 class _AppLocalizationsDelegate
     extends LocalizationsDelegate<AppLocalizations> {
   const _AppLocalizationsDelegate();
 
+  /// todo: missing documentation
   @override
   bool isSupported(Locale locale) {
     return ['en', 'de'].contains(locale.languageCode);
   }
 
+  /// todo: missing documentation
   @override
   Future<AppLocalizations> load(Locale locale) async {
     AppLocalizations localizations = new AppLocalizations(locale);
@@ -50,6 +61,7 @@ class _AppLocalizationsDelegate
     return localizations;
   }
 
+  /// todo: missing documentation
   @override
   bool shouldReload(_AppLocalizationsDelegate old) => false;
 }
