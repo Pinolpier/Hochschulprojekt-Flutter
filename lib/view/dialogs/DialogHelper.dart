fbb684e4bb25558ffad9fad4eb320ab6f6d35dc0
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-15 15:49:24 +0200

    corrected DialogHelper.dart documentation

--- lib/view/dialogs/DialogHelper.dart
+++ lib/view/dialogs/DialogHelper.dart
@@ -8,12 +8,25 @@ import 'addFriends_dialog.dart';
 import 'changeBioOrGroupName_dialog.dart';
 import 'friendList_dialog.dart';
 
+/// todo: add author
+/// todo: this is no class
 /// Helper class that calls the different dialogs when the buttons were pressed and displays them
 
-  showFriendsDialogEvent(context, Event event) => showDialog(context: context, builder: (context) => FriendslistdialogScreen(event));
-  showFriendsDialog(context) => showDialog(context: context, builder: (context) => FriendslistdialogScreen.create());
-  showAddFriendsDialog(context) => showDialog(context: context, builder: (context) => AddFriendsDialogScreen());
-  showChangeBioDialog(context, UserProfile userProfile) => showDialog(context: context, builder: (context) => ChangeBioDialog(userProfile));
-  showChangeGroup(context) => showDialog(context: context, builder: (context) => ChangeBioDialog.create());
-  showErrorDialog(context, String title, String body, bool isHighPrio) => showDialog(barrierDismissible: false, context: context, builder: (context) => ErrorDialogCreator(title,body,isHighPrio));
-  showProfileScreen(context, String uid) => showDialog(context: context, builder: (context) => ProfileScreen(uid));
+/// todo: missing documentation
+showFriendsDialogEvent(context, Event event) => showDialog(
+    context: context, builder: (context) => FriendslistdialogScreen(event));
+showFriendsDialog(context) => showDialog(
+    context: context, builder: (context) => FriendslistdialogScreen.create());
+showAddFriendsDialog(context) => showDialog(
+    context: context, builder: (context) => AddFriendsDialogScreen());
+showChangeBioDialog(context, UserProfile userProfile) => showDialog(
+    context: context, builder: (context) => ChangeBioDialog(userProfile));
+showChangeGroup(context) => showDialog(
+    context: context, builder: (context) => ChangeBioDialog.create());
+showErrorDialog(context, String title, String body, bool isHighPrio) =>
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => ErrorDialogCreator(title, body, isHighPrio));
+showProfileScreen(context, String uid) =>
+    showDialog(context: context, builder: (context) => ProfileScreen(uid));
