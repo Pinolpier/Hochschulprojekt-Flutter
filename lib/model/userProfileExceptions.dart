79a67e33970ef3a4c4341a0653409a868e0b43ee
eoao <mdarscht@stud.hs-heilbronn.de>
2020-06-15 13:06:32 +0200

    corrected userProfileException.dart, completed model package

--- lib/model/userProfileExceptions.dart
+++ lib/model/userProfileExceptions.dart
@@ -1,13 +1,18 @@
+/// todo: add author
+/// todo: use documentation comments like -> /// documentation comment
+/// todo: CONSIDER writing a library-level doc comment
 /*
 In Dart an exception doesn't need to inherit from Exception, I decided not to do so, because
 1) I want the the original Exception that has happened in the backend to be attached to my BackendException, but if it's final I can't build a constructor that fits Exception's needs
  */
 class UserProfileException implements Exception {
+  /// todo: add documentation of variables
   final Exception _originalException;
   final String _message;
 
   const UserProfileException(this._originalException, this._message);
 
+  /// todo: missing documentation
   String toString() {
     return (_message != null
             ? _message
@@ -23,39 +28,47 @@ class UserProfileException implements Exception {
   }
 }
 
+/// todo: missing documentation
 class UserNotSignedInException extends UserProfileException {
   const UserNotSignedInException(Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class NullArgumentException extends UserProfileException {
   const NullArgumentException(Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class ForeignProfileAccessForbiddenException extends UserProfileException {
   const ForeignProfileAccessForbiddenException(
       Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class PermissionDeniedException extends UserProfileException {
   const PermissionDeniedException(Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class IllegalDatabaseStateException extends UserProfileException {
-  const IllegalDatabaseStateException(Exception originalException,
-      String message)
+  const IllegalDatabaseStateException(
+      Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class NoUserProfileFoundException extends UserProfileException {
   const NoUserProfileFoundException(Exception originalException, String message)
       : super(originalException, message);
 }
 
+/// todo: missing documentation
 class UniqueConstraintViolatedException extends UserProfileException {
-  const UniqueConstraintViolatedException(Exception originalException, String message)
+  const UniqueConstraintViolatedException(
+      Exception originalException, String message)
       : super(originalException, message);
 }
