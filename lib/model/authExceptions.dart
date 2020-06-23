e78b2becb1e9cca5210a3b072aa33def8caac4e1
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2020-06-24 01:43:01 +0200

    did documentation for authExceptions.dart

--- lib/model/authExceptions.dart
+++ lib/model/authExceptions.dart
@@ -1,18 +1,22 @@
-/// todo: documentation comments are like these: -> /// documenation comment
-/// todo: add author
+/// author Markus Link
 /// todo: CONSIDER writing a library-level doc comment
-/*
-In Dart an exception doesn't need to inherit from Exception, I decided not to do so, because
-1) I want the the original Exception that has happened in the backend to be attached to my BackendException, but if it's final I can't build a constructor that fits Exception's needs
- */
+/// --> What
+
+/// In Dart an exception doesn't need to inherit from Exception
+///
+/// I decided not to do so, because
+/// 1) I want the the original Exception that has happened in the backend to be attached to my BackendException,
+/// but if it's final I can't build a constructor that fits Exception's needs
 class AuthException implements Exception {
   /// todo: missing documentation about variables
+  /// --> What (sprechende Namen!)
   final Exception _originalException;
   final String _message;
 
   const AuthException(this._originalException, this._message);
 
   /// todo: missing documentation
+  /// --> What
   String toString() {
     return (_message != null
             ? _message
@@ -29,36 +33,42 @@ class AuthException implements Exception {
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class UserEmailNotVerified extends AuthException {
   const UserEmailNotVerified(Exception originalException, String message)
       : super(originalException, message);
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class UserDisabledException extends AuthException {
   const UserDisabledException(Exception originalException, String message)
       : super(originalException, message);
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class NotAnEmailException extends AuthException {
   const NotAnEmailException(Exception originalException, String message)
       : super(originalException, message);
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class WrongPasswordException extends AuthException {
   const WrongPasswordException(Exception originalException, String message)
       : super(originalException, message);
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class UserNotFoundException extends AuthException {
   const UserNotFoundException(Exception originalException, String message)
       : super(originalException, message);
 }
 
 /// todo: missing documentation
+/// --> What (sprechende Namen!)
 class SignInAbortedException extends AuthException {
   const SignInAbortedException(Exception originalException, String message)
       : super(originalException, message);
